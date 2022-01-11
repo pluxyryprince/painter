4eaded018d90b2094397f050adc316024dfd6e58
pluxury golf <pluxury golf@DESKTOP-SV9M877>
2022-01-11 13:53:44 +0300

    комментарии

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,62 +20,63 @@ namespace painter
     /// </summary>
     public partial class MainWindow : Window
     {
-        Brush Color; 
-        bool IsDraw;
-        double R;
+        Brush Color; //объект для цвета
+        bool IsDraw; //нажата ли кнопка мыши
+        double R; //толщина линии
 
         public MainWindow()
         {
             InitializeComponent();
-            IsDraw = false;
-            R = 1;
-            Color = Brushes.Red;
+            IsDraw = false; //изначально перо не опущено на холст
+            R = 1;//размер линии = 1 пиксель
+            Color = Brushes.Red;//по умолчанию цвет пера красный
 
         }
 
-   
-        private void cmRed(object sender, RoutedEventArgs e)
+        //обработчик нажатия на радиокнопки
+        private void cmRed(object sender, RoutedEventArgs e)//выбор красног8о цвета
         {
             Color = Brushes.Red;
             rectColor.Fill = Color;
         }
 
-        private void cmBlue(object sender, RoutedEventArgs e)
+        private void cmBlue(object sender, RoutedEventArgs e)//выбор синего цвета пера
         {
             Color = Brushes.Blue;
             rectColor.Fill = Color;
         }
 
-        private void cmGreen(object sender, RoutedEventArgs e)
+        private void cmGreen(object sender, RoutedEventArgs e)//выбор зеленого цвета пера
         {
             Color = Brushes.Green;
             rectColor.Fill = Color;
         }
 
-        private void cmClear(object sender, RoutedEventArgs e)
+        private void cmClear(object sender, RoutedEventArgs e)//очистить холст
         {
             g.Children.Clear();
         }
 
-        private void cmSize(object sender, RoutedPropertyChangedEventArgs<double> e)
+        private void cmSize(object sender, RoutedPropertyChangedEventArgs<double> e)//изменение размера линии
         {
             R = slSize.Value;
         }
 
-        private void cmDown(object sender, MouseButtonEventArgs e)
+        private void cmDown(object sender, MouseButtonEventArgs e)//если кнопка мыши нажата
         {
-            IsDraw = true;
+            IsDraw = true;//опустить перо
         }
 
-        private void cmUp(object sender, MouseButtonEventArgs e)
+        private void cmUp(object sender, MouseButtonEventArgs e)//если кнопка мыши не нажата
         {
-            IsDraw = false;
+            IsDraw = false;//поднять перо
         }
 
-        private void cmMove(object sender, MouseEventArgs e)
+        private void cmMove(object sender, MouseEventArgs e)//рисование
         {
-            if (IsDraw)
+            if (IsDraw)//если перо опущено
             {
+                //если указатель мыши находится вне холста
                 if((e.GetPosition(g).X < 0) || (e.GetPosition(g).X > g.Width))
                 {
                     return;
@@ -85,12 +86,13 @@ namespace painter
                     return;
                 }
 
-                Ellipse o = new Ellipse();
-                o.Width = R;
-                o.Height = R;
-                o.Fill = Color;
-                o.Margin = new Thickness(e.GetPosition(g).X - R / 2, e.GetPosition(g).Y - R / 2, 0, 0);
-                g.Children.Add(o);
+                //если указатель находится в пределах холста
+                Ellipse o = new Ellipse();//создаем объект круг
+                o.Width = R;//ширина
+                o.Height = R;//высота
+                o.Fill = Color;//цвет
+                o.Margin = new Thickness(e.GetPosition(g).X - R / 2, e.GetPosition(g).Y - R / 2, 0, 0);//положение объекта под указателем мыши
+                g.Children.Add(o);//добавить объект на холст
             }
         }
     }
